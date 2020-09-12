7fc3420fde9ce3c5f7ae90c38c2c9f2982cd706e
Aaron Bowes <aaron.bowes@outlook.com>
2020-09-12 18:37:48 -0400

    Add transitions to menu and gallery

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 let menuButton = document.querySelector('#menu');
-let links = document.querySelector('#links');
+let menu = document.querySelector('#links');
 menuButton.addEventListener('click', () => {
-	links.classList.toggle('display');
+	menu.classList.toggle('display');
 });
+
+let gallery = document.querySelector('.gallery');
+let photo = document.querySelectorAll('.gallery-photo');
+for (let i = 0; i < photo.length; i++) {
+	photo[i].addEventListener('click', (e) => {
+		photo[i].classList.toggle('full-screen-photo');
+	})
+}
